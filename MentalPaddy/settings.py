3c7970c59e4cb54f7b272563588086bc875e29ac
Jerry <jegoisgs66@gmail.com>
2024-03-01 04:03:18 +0100

    Update on profile

--- MentalPaddy/settings.py
+++ MentalPaddy/settings.py
@@ -104,7 +104,6 @@ DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
